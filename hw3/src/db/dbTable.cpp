cbba4f720b4d7074b26f65c54600b0f0612c2163
Andy Pan <andy0130tw@yahoo.com.tw>
2015-10-10 03:52:52 +0800

    hw3: initial commit

---
+++ hw3/src/db/dbTable.cpp
@@ -0,0 +1,158 @@
+/****************************************************************************
+  FileName     [ dbTable.cpp ]
+  PackageName  [ db ]
+  Synopsis     [ Define database Table member functions ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2015-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#include <iomanip>
+#include <string>
+#include <cctype>
+#include <cassert>
+#include <set>
+#include <algorithm>
+#include "dbTable.h"
+
+using namespace std;
+
+/*****************************************/
+/*          Global Functions             */
+/*****************************************/
+ostream& operator << (ostream& os, const DBRow& r)
+{
+   // TODO: to print out a row.
+   // - Data are seperated by a space. No trailing space at the end.
+   // - Null cells are printed as '.'
+   return os;
+}
+
+ostream& operator << (ostream& os, const DBTable& t)
+{
+   // TODO: to print out a table
+   // - Data are seperated by setw(6) and aligned right.
+   // - Null cells should be left blank (printed as ' ').
+   return os;
+}
+
+ifstream& operator >> (ifstream& ifs, DBTable& t)
+{
+   // TODO: to read in data from csv file and store them in a table
+   // - You can assume all the data of the table are in a single line.
+   return ifs;
+}
+
+/*****************************************/
+/*   Member Functions for class DBRow    */
+/*****************************************/
+void
+DBRow::removeCell(size_t c)
+{
+   // TODO
+}
+
+/*****************************************/
+/*   Member Functions for struct DBSort  */
+/*****************************************/
+bool
+DBSort::operator() (const DBRow& r1, const DBRow& r2) const
+{
+   // TODO: called as a functional object that compares the data in r1 and r2
+   //       based on the order defined in _sortOrder
+   return false;
+}
+
+/*****************************************/
+/*   Member Functions for class DBTable  */
+/*****************************************/
+void
+DBTable::reset()
+{
+   // TODO
+}
+
+void
+DBTable::addCol(const vector<int>& d)
+{
+   // TODO: add a column to the right of the table. Data are in 'd'.
+}
+
+void
+DBTable::delRow(int c)
+{
+   // TODO: delete row #c. Note #0 is the first row.
+}
+
+void
+DBTable::delCol(int c)
+{
+   // delete col #c. Note #0 is the first row.
+   for (size_t i = 0, n = _table.size(); i < n; ++i)
+      _table[i].removeCell(c);
+}
+
+// For the following getXXX() functions...  (except for getCount())
+// - Ignore null cells
+// - If all the cells in column #c are null, return NAN
+// - Return "float" because NAN is a float.
+float
+DBTable::getMax(size_t c) const
+{
+   // TODO: get the max data in column #c
+   return 0.0;
+}
+
+float
+DBTable::getMin(size_t c) const
+{
+   // TODO: get the min data in column #c
+   return 0.0;
+}
+
+float 
+DBTable::getSum(size_t c) const
+{
+   // TODO: compute the sum of data in column #c
+   return 0.0;
+}
+
+int
+DBTable::getCount(size_t c) const
+{
+   // TODO: compute the number of distinct data in column #c
+   // - Ignore null cells
+   return 0;
+}
+
+float
+DBTable::getAve(size_t c) const
+{
+   // TODO: compute the average of data in column #c
+   return 0.0;
+}
+
+void
+DBTable::sort(const struct DBSort& s)
+{
+   // TODO: sort the data according to the order of columns in 's'
+}
+
+void
+DBTable::printCol(size_t c) const
+{
+   // TODO: to print out a column.
+   // - Data are seperated by a space. No trailing space at the end.
+   // - Null cells are printed as '.'
+}
+
+void
+DBTable::printSummary() const
+{
+   size_t nr = nRows(), nc = nCols(), nv = 0;
+   for (size_t i = 0; i < nr; ++i)
+      for (size_t j = 0; j < nc; ++j)
+         if (_table[i][j] != INT_MAX) ++nv;
+   cout << "(#rows, #cols, #data) = (" << nr << ", " << nc << ", "
+        << nv << ")" << endl;
+}
+
