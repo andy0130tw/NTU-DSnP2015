5ff51e1ade2f76a763d16d0b716d8935429cd141
Andy Pan <andy0130tw@yahoo.com.tw>
2015-11-15 19:34:23 +0800

    hw5: initial commit

---
+++ hw5/src/util/dlist.h
@@ -0,0 +1,97 @@
+/****************************************************************************
+  FileName     [ dlist.h ]
+  PackageName  [ util ]
+  Synopsis     [ Define doubly linked list package ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2005-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#ifndef DLIST_H
+#define DLIST_H
+
+#include <cassert>
+
+template <class T> class DList;
+
+// DListNode is supposed to be a private class. User don't need to see it.
+// Only DList and DList::iterator can access it.
+//
+// DO NOT add any public data member or function to this class!!
+//
+template <class T>
+class DListNode
+{
+   friend class DList<T>;
+   friend class DList<T>::iterator;
+
+   DListNode(const T& d, DListNode<T>* p = 0, DListNode<T>* n = 0):
+      _data(d), _prev(p), _next(n) {}
+
+   T              _data;
+   DListNode<T>*  _prev;
+   DListNode<T>*  _next;
+};
+
+
+template <class T>
+class DList
+{
+public:
+   DList() {
+      _head = new DListNode<T>(T());
+      _head->_prev = _head->_next = _head; // _head is a dummy node
+   }
+   ~DList() { clear(); delete _head; }
+
+   // DO NOT add any more data member or function for class iterator
+   class iterator
+   {
+      friend class DList;
+
+   public:
+      iterator(DListNode<T>* n= 0): _node(n) {}
+      iterator(const iterator& i) : _node(i._node) {}
+      ~iterator() {} // Should NOT delete _node
+
+      // TODO: implement these overloaded operators
+      const T& operator * () const { return *(this); }
+      T& operator * () { return _node->_data; }
+      iterator& operator ++ () { return *(this); }
+      iterator operator ++ (int) { return *(this); }
+      iterator& operator -- () { return *(this); }
+      iterator operator -- (int) { return *(this); }
+
+      iterator& operator = (const iterator& i) { return *(this); }
+
+      bool operator != (const iterator& i) const { return false; }
+      bool operator == (const iterator& i) const { return false; }
+
+   private:
+      DListNode<T>* _node;
+   };
+
+   // TODO: implement these functions
+   iterator begin() const { return 0; }
+   iterator end() const { return 0; }
+   bool empty() const { return false; }
+   size_t size() const {  return 0; }
+
+   void push_back(const T& x) { }
+   void pop_front() { }
+   void pop_back() { }
+
+   // return false if nothing to erase
+   bool erase(iterator pos) { return false; }
+   bool erase(const T& x) { return false; }
+
+   void clear() { }  // delete all nodes except for the dummy node
+
+   void sort() const { }
+
+private:
+   DListNode<T>*  _head;  // = dummy node if list is empty
+
+   // [OPTIONAL TODO] helper functions; called by public member functions
+};
+
+#endif // DLIST_H
