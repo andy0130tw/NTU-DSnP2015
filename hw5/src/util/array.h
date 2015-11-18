ba3b4e49c5bd7fcc7c5e45ab029a0c54101d2547
Andy Pan <andy0130tw@yahoo.com.tw>
2015-11-19 02:17:45 +0800

    finished (unoptimized) dynamic array

--- hw5/src/util/array.h
+++ hw5/src/util/array.h
@@ -34,42 +34,76 @@ public:
       ~iterator() {} // Should NOT delete _node
 
       // TODO: implement these overloaded operators
-      const T& operator * () const { return (*this); }
+      const T& operator * () const { return (*_node); }
       T& operator * () { return (*_node); }
-      iterator& operator ++ () { return (*this); }
-      iterator operator ++ (int) { return (*this); }
-      iterator& operator -- () { return (*this); }
-      iterator operator -- (int) { return (*this); }
+      iterator& operator ++ () { ++_node; return (*this); }
+      iterator operator ++ (int) { iterator ret = *(this); ++ *(this); return ret; }
+      iterator& operator -- () { --_node; return (*this); }
+      iterator operator -- (int) { iterator ret = *(this); -- *(this); return ret; }
 
-      iterator operator + (int i) const { return (*this); }
-      iterator& operator += (int i) { return (*this); }
+      iterator operator + (int i) const { return _node + i; }
+      iterator& operator += (int i) { _node+=i; return *(this); }
 
-      iterator& operator = (const iterator& i) { return (*this); }
+      iterator& operator = (const iterator& i) { _node = i._node; return (*this); }
 
-      bool operator != (const iterator& i) const { return false; }
-      bool operator == (const iterator& i) const { return false; }
+      bool operator != (const iterator& i) const { return !(*(this) == i); }
+      bool operator == (const iterator& i) const { return (_node == i._node); }
 
    private:
       T*    _node;
    };
 
    // TODO: implement these functions
-   iterator begin() const { return 0; }
-   iterator end() const { return 0; }
-   bool empty() const { return false; }
-   size_t size() const { return 0; }
+   iterator begin() const { return _data; }
+   iterator end() const { return _data + _size; }
+   bool empty() const { return (_size == 0); }
+   size_t size() const { return _size; }
 
-   T& operator [] (size_t i) { return _data[0]; }
-   const T& operator [] (size_t i) const { return _data[0]; }
+   T& operator [] (size_t i) { return _data[i]; }
+   const T& operator [] (size_t i) const { return _data[i]; }
 
-   void push_back(const T& x) { }
-   void pop_front() { }
-   void pop_back() { }
+   void push_back(const T& x) {
+      if (_size == _capacity)
+         /*bool status = */
+         redim(_size + 1);
+         // if (!status) throw bad_alloc();
+      _data[_size++] = x;
+   }
+   void pop_front() {
+      if (empty()) return;
+      for (size_t i = 1; i < _size; i++) {
+         _data[i-1] = _data[i];
+      }
+      --_size;
+   }
+   void pop_back() {
+      if (empty()) return;
+      --_size;
+   }
 
-   bool erase(iterator pos) { return false; }
-   bool erase(const T& x) { return false; }
+   bool erase(iterator pos) {
+      if (empty()) return false;
+      iterator it = pos + 1, prev = pos;
+      while (it != end()) {
+         *(prev._node) = *(it._node);
+         prev = it;
+         ++it;
+      }
+      --_size;
+      return true;
+   }
+   bool erase(const T& x) {
+      if (empty()) return false;
+      for (iterator it = begin(); it != end(); ++it) {
+         if (*(it._node) == x) {
+            erase(it);
+            return true;
+         }
+      }
+      return false;
+   }
 
-   void clear() { }
+   void clear() { _size = 0; }
 
    // This is done. DO NOT change this one.
    void sort() const { if (!empty()) ::sort(_data, _data+_size); }
@@ -84,6 +118,26 @@ private:
    size_t       _capacity;   // max number of elements
 
    // [OPTIONAL TODO] Helper functions; called by public member functions
+   size_t toCapacity(size_t i) {
+      size_t ret = 1;
+      while (i > ret) ret *= 2;
+      return ret;
+   }
+
+   bool redim(size_t newCap) {
+      newCap = toCapacity(newCap);
+      assert(newCap >= _size);
+      T* newData = new T[newCap];
+      if (!newData) return false;
+      for (size_t i = 0;i < _size; i++)
+         newData[i] = _data[i];
+
+      _capacity = newCap;
+      // replace the old one
+      delete [] _data;
+      _data = newData;
+      return true;
+   }
 };
 
 #endif // ARRAY_H
